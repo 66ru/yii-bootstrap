74df001e2207d2f391de523bcba1182c21a6f429
Crisu83 <none@none>
2012-12-19 03:06:32 +0200

    Fix some bugs in affix, alert and collapse widgets

--- widgets/TbAlert.php
+++ widgets/TbAlert.php
@@ -115,8 +115,10 @@ class TbAlert extends CWidget
 
 				if ($this->closeText !== false && !isset($alert['closeText']))
 					$alert['closeText'] = $this->closeText;
+				else
+					$alert['closeText'] = false;
 
-				if (isset($alert['closeText']) && $alert['closeText'] !== false)
+				if ($alert['closeText'] !== false)
 					echo '<a class="close" data-dismiss="alert">'.$alert['closeText'].'</a>';
 
 				echo Yii::app()->user->getFlash($type);
