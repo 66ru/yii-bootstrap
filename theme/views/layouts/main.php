9689b86d0a1219d42fd9b8ad4e18055f525475e1
Crisu83 <none@none>
2012-12-21 22:43:40 +0200

    Fix issue with publishing assets in the Bootstrap component
    --HG--
    branch : stable

--- theme/views/layouts/main.php
+++ theme/views/layouts/main.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+	<?php Yii::app()->bootstrap->register(); ?>
 </head>
 
 <body>
